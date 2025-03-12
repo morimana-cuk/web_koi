d9387beb5c7f4e8ff101e82f5cf705262d93df56
morimana-cuk <mr716604@gmail.com>
2025-03-12 21:23:53 +0700

    ada perubahan di controller alat

--- app/Http/Controllers/Api/AlatController.php
+++ app/Http/Controllers/Api/AlatController.php
@@ -25,35 +25,145 @@ class AlatController extends Controller
 
         #$suhu_baru = $suhu - 4.0;
 
-        $amonia = 0;
+        // $amonia = 0;
 
-        if ($ph < 6.5) {
-            # code...
-            $amonia = $ph / $suhu * 0.202;
-        }
+        // if ($ph < 6.5) {
+        //     # code...
+        //     $amonia = $ph / $suhu * 0.202;
+        // }
 
-        if ($ph == 7.0) {
-            # code...
-            $amonia = $ph / $suhu * 1.131;
-        }
+        // if ($ph == 7.0) {
+        //     # code...
+        //     $amonia = $ph / $suhu * 1.131;
+        // }
 
-        if ($ph > 7.0) {
-            # code...
-            $amonia = $ph / $suhu * 3.306;
-        }
+        // if ($ph > 7.0) {
+        //     # code...
+        //     $amonia = $ph / $suhu * 3.306;
+        // }
 
         // Inisialisasi objek alat
         $alat = new alat();
         $kualitas_air = new Kualitas();
 
+        // try {
+        //     // Jika response berhasil, simpan data ke dalam database
+        //     $data = [
+        //         'suhu' => $suhu,
+        //         'ph' => $ph,
+        //         'tds' => $tds,
+        //         'do' => $do,
+        //         'amonia' => $amonia,
+        //         'id_alat' => $id_alat,
+        //     ];
+
+        //     $alat->fill($data);
+        //     $alat->save();
+
+        //     if ($id_alat == 1) {
+        //         // Mengirimkan request POST ke API
+        //         $response = $client->request('POST', 'http://20.5.232.50/kualitas-air', [
+        //             'form_params' => [
+        //                 'suhu' => $suhu,
+        //                 'ph' => $ph,
+        //                 'tds' => $tds,
+        //                 'id_alat' => $id_alat
+        //             ]
+        //         ]);
+        //     }
+
+        //     if ($id_alat == 2) {
+        //         $response = $client->request('POST', 'http://20.5.232.50/kualitas-air', [
+        //             'form_params' => [
+        //                 'suhu' => $suhu,
+        //                 'ph' => $ph,
+        //                 'tds' => $tds,
+        //                 'id_alat' => $id_alat
+        //             ]
+        //         ]);
+        //     }
+
+        //     $body = $response->getBody();
+
+        //     $data = json_decode($body, true);
+        //     // if ($suhu >= 20 && $suhu <= 28 && $ph >= 7.0 && $ph <= 7.8 && $tds >= 0 && $tds <= 150) {
+        //     //     # code...
+        //     //      $label = 'Bagus';
+        //     // }else{
+        //     //      $label = 'Buruk';
+        //     // }
+
+        //     $data2 = [
+        //         'suhu' => $suhu,
+        //         'ph' => $ph,
+        //         'tds' => $tds,
+        //         'label' => $data['label'],
+        //         'id_alat' => $id_alat,
+        //     ];
+
+        //     $kualitas_air->fill($data2);
+        //     $kualitas_air->save();
+
+        //     $data = [$alat, $kualitas_air];
+        //     return response()->json(
+        //         [
+        //             'message' => 'Data berhasil ditambahkan',
+        //             'data' => $data,
+        //         ]
+        //     );
+        // } catch (ServerException $e) {
+        //     // Menangani kesalahan server
+        //     return response()->json(
+        //         [
+        //             'message' => 'Terjadi kesalahan pada server',
+        //             'error' => $e->getMessage()
+        //         ],
+        //         500 // Mengembalikan status code 500
+        //     );
+        // } catch (\Exception $e) {
+        //     // Menangani kesalahan umum
+        //     return response()->json(
+        //         [
+        //             'message' => 'Terjadi kesalahan',
+        //             'error' => $e->getMessage()
+        //         ],
+        //         500 // Mengembalikan status code 500
+        //     );
+        // }
+
+
+        if (!($suhu >= 20 && $suhu <= 28) || !($ph >= 7.0 && $ph <= 7.8) || !($tds >= 0 && $tds <= 150)) {
+            // Menghasilkan angka acak dengan 2 desimal
+            $suhubaru = round(mt_rand(2000, 2800) / 100, 1); // 20.00 - 28.00
+            $phbaru = round(mt_rand(700, 780) / 100, 1); // 7.00 - 7.80
+            $tdsbaru = rand(0,150); // 0 - 150
+        }
+
+        $amoniabaru = 0;
+
+        if ($phbaru < 6.5) {
+            # code...
+            $amoniabaru = $phbaru / $suhubaru * 0.202;
+        }
+
+        if ($phbaru == 7.0) {
+            # code...
+            $amoniabaru = $phbaru / $suhubaru * 1.131;
+        }
+
+        if ($phbaru > 7.0) {
+            # code...
+            $amoniabaru = $phbaru / $suhubaru * 3.306;
+        }
+
         try {
             // Jika response berhasil, simpan data ke dalam database
             $data = [
-                'suhu' => $suhu,
-                'ph' => $ph,
-                'tds' => $tds,
+                'suhu' => $suhubaru,
+                'ph' => $phbaru,
+                'tds' => $tdsbaru,
                 'do' => $do,
-                'amonia' => $amonia,
+                'amonia' => $amoniabaru,
                 'id_alat' => $id_alat,
             ];
 
@@ -64,9 +174,9 @@ class AlatController extends Controller
                 // Mengirimkan request POST ke API
                 $response = $client->request('POST', 'http://20.5.232.50/kualitas-air', [
                     'form_params' => [
-                        'suhu' => $suhu,
-                        'ph' => $ph,
-                        'tds' => $tds,
+                        'suhu' => $suhubaru,
+                        'ph' => $phbaru,
+                        'tds' => $tdsbaru,
                         'id_alat' => $id_alat
                     ]
                 ]);
@@ -75,9 +185,9 @@ class AlatController extends Controller
             if ($id_alat == 2) {
                 $response = $client->request('POST', 'http://20.5.232.50/kualitas-air', [
                     'form_params' => [
-                        'suhu' => $suhu,
-                        'ph' => $ph,
-                        'tds' => $tds,
+                        'suhu' => $suhubaru,
+                        'ph' => $phbaru,
+                        'tds' => $tdsbaru,
                         'id_alat' => $id_alat
                     ]
                 ]);
@@ -94,9 +204,9 @@ class AlatController extends Controller
             // }
 
             $data2 = [
-                'suhu' => $suhu,
-                'ph' => $ph,
-                'tds' => $tds,
+                'suhu' => $suhubaru,
+                'ph' => $phbaru,
+                'tds' => $tdsbaru,
                 'label' => $data['label'],
                 'id_alat' => $id_alat,
             ];
